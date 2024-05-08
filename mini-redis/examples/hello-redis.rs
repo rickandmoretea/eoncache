4327a09f6ad5308ad11f5da7b766e2eca55952cf
Pongchai Hantapong <pongchaihantapong@Pongchais-MacBook-Air.local>
2024-05-08 22:22:10 +0700

    Implemented shared stated HashMap with Mutex

--- mini-redis/examples/hello-redis.rs
+++ mini-redis/examples/hello-redis.rs
@@ -3,7 +3,7 @@ use mini_redis::{client, Result};
 #[tokio::main]
 async fn main() -> Result<()> {
     // Open a connection to the mini-redis address.
-    let mut client = client::connect("127.0.0.1:6379").await?;
+    let mut client = client::connect("127.0.0.1:6381").await?;
 
     // Set the key "hello" with value "world"
     client.set("hello", "world".into()).await?;
--- mini-redis/src/main.rs
+++ mini-redis/src/main.rs
@@ -1,43 +1,59 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use mini_redis::Command::{self, Get, Set};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
-    let listener:TcpListener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    println!("Listening");
+
+    let db: Db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        // Clone the handle to the hash map.
+        let db = db.clone();
 
+        println!("Accepted");
         tokio::spawn(async move {
-             process(socket).await;
+            process(socket, db).await;
         });
     }
 }
+async fn process(socket: TcpStream, db: Db) {
 
-async fn process(socket: TcpStream) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-    
-    let mut db = HashMap::new();
 
+    // Connection, provided by `mini-redis`, handles parsing frames from
+    // the socket
     let mut connection = Connection::new(socket);
 
+    // Use `read_frame` to receive a command from the connection.
     while let Some(frame) = connection.read_frame().await.unwrap() {
-        let response = match Command::from_frame(frame).unwrap() { 
+        let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
             }
             cmd => panic!("unimplemented {:?}", cmd),
         };
+
+        // Write the response to the client
         connection.write_frame(&response).await.unwrap();
     }
-
 }
